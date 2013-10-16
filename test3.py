733b8480813b7db91fcc98ab8d34249325125606
yxyang <carljohnson40@hotmail.com>
2013-10-16 13:59:04 -0400

    *.jason

--- test3.py
+++ test3.py
@@ -35,7 +35,7 @@ def allWord(email):
 print 'loading'
 sc = SparkContext("spark://ec2-54-205-79-23.compute-1.amazonaws.com:7077", "Simple App")
 # Replace `lay-k.json` with `*.json` to get a whole lot more data.
-lay = sc.textFile('s3n://AKIAJFDTPC4XX2LVETGA:lJPMR8IqPw2rsVKmsSgniUd+cLhpItI42Z6DCFku@6885public/enron/lay-k.json')
+lay = sc.textFile('s3n://AKIAJFDTPC4XX2LVETGA:lJPMR8IqPw2rsVKmsSgniUd+cLhpItI42Z6DCFku@6885public/enron/*.json')
 
 json_lay = lay.map(lambda x: json.loads(x)).cache()
 print 'json lay count', json_lay.count()
